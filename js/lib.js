f083fb63caa3bf0ae48832b96a2edf6d7769edd3
Toby Sullivan <tobyjsullivan@gmail.com>
2014-10-04 23:59:16 -0700

    Fix pwd program

--- js/lib.js
+++ js/lib.js
@@ -2,8 +2,9 @@ const lib = {
   help: function() {
     print_line("Commands available:");
     print_line("HELP - display this help");
+    print_line("PWD - print current directory");
   },
-  ls: function() {
+  pwd: function() {
     for(var i = 0; i < path.length; i++) {
       print_line("/"+path[i].name);
     }
